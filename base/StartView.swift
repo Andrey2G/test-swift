cf45f13b35cf52866c7dd6895d47c6d263287e76
Andrey <andrey.grunyov@gmail.com>
2023-01-28 17:17:16 +0100

    Add StartView

---
+++ base/StartView.swift
@@ -0,0 +1,39 @@
+//
+//  StartView.swift
+//  base
+//
+//  Created by user229277 on 1/28/23.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image("Launch")
+                .resizable()
+            
+            HStack {
+                Button("START") {
+                    
+                }
+                
+                Spacer()
+                
+                Button("MORE"){
+                    
+                }
+            }
+
+
+            
+        }
+        .padding()
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
