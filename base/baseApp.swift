1f03a5ef737d09e8b99099fd68cdcf97dc344f82
Andrey <andrey.grunyov@gmail.com>
2023-01-28 15:55:19 +0100

    first commit

---
+++ base/baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  baseApp.swift
+//  base
+//
+//  Created by user229277 on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
