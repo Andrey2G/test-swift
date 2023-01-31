791da403cd0fa8852f3c9dd8d972790419a7b020
Andrey <andrey.grunyov@gmail.com>
2023-01-31 12:20:56 +0100

    start ad of AdMob

--- base/ChaptersList.swift
+++ base/ChaptersList.swift
@@ -12,33 +12,39 @@ struct ChaptersList: View {
     init() {
         
                 UITableView.appearance().backgroundColor = UIColor(Color.clear)
-            
+        //showIntersitialAd.toggle()
     }
+    
     var body: some View{
-        VStack{
-            Text("CII R04 Pensions & Retirement Planning")
-            NavigationView {
-                List(chapters) { chapter in
-                    ZStack {
-                        Button("") {}
-                        NavigationLink(destination:
-                                        StartTestView(chapter: chapter.title)
-                                       
-                        )
-                        {
-                            ChapterRow(chapter: chapter)
-                            
-                            
-                        }
-                    }.background(Color(hex: "bad859"))
-                        .cornerRadius(10)
-                        //.listRowBackground(Color.blue)
-                    
+        ZStack {
+            VStack{
+                Text("CII R04 Pensions & Retirement Planning")
+                NavigationView {
+                    List(chapters) { chapter in
+                        ZStack {
+                            Button("") {}
+                            NavigationLink(destination:
+                                            StartTestView(chapter: chapter.title)
+                                           
+                            )
+                            {
+                                ChapterRow(chapter: chapter)
+                                
+                                
+                            }
+                        }.background(Color(hex: "bad859"))
+                            .cornerRadius(10)
+                            //.listRowBackground(Color.blue)
+                        
+                    }
+                    .onAppear {       UITableView.appearance().separatorStyle = .none     }
+                    //.listRowSeparator(.hidden)
                 }
-                .onAppear {       UITableView.appearance().separatorStyle = .none     }
-                //.listRowSeparator(.hidden)
             }
-        }.presentInterstitialAd(isPresented: $showIntersitialAd,  adUnitId: "ca-app-pub-3940256099942544/4411468910")
+            Button("ad"){self.showIntersitialAd.toggle()}
+        }
+        
+        .presentInterstitialAd(isPresented: $showIntersitialAd,  adUnitId: "ca-app-pub-3940256099942544/4411468910")
     }
 }
 
--- base/FullScreenModifier.swift
+++ base/FullScreenModifier.swift
@@ -9,14 +9,7 @@ import SwiftUI
 
 struct FullScreenModifier<Parent: View>: View {
     @Binding var isPresented: Bool
-    @State var adType: AdType
     
-    enum AdType {
-        case interstitial
-        case rewarded
-    }
-    
-    var rewardFunc: () -> Void
     var adUnitId: String
     var parent: Parent
     
@@ -27,30 +20,20 @@ struct FullScreenModifier<Parent: View>: View {
             if isPresented {
                 EmptyView()
                     .edgesIgnoringSafeArea(.all)
-                
-                if adType == .rewarded {
-                    //RewardedAdView(isPresented: $isPresented, adUnitId: adUnitId, rewardFunc: rewardFunc).edgesIgnoringSafeArea(.all)
-                } else if adType == .interstitial {
                     InterstitialAdView(isPresented: $isPresented, adUnitId: adUnitId)
-                }
+                
             }
         }
         .onAppear {
-            if adType == .rewarded {
-                //RewardedAd.shared.loadAd(withAdUnitId: adUnitId)
-            } else if adType == .interstitial {
-                InterstitialAd.shared.loadAd(withAdUnitId: adUnitId)
-            }
+            InterstitialAd.shared.loadAd(withAdUnitId: adUnitId)
+            
         }
     }
 }
 
 extension View {
-    public func presentRewardedAd(isPresented: Binding<Bool>, adUnitId: String, rewardFunc: @escaping (() -> Void)) -> some View {
-        FullScreenModifier(isPresented: isPresented, adType: .rewarded, rewardFunc: rewardFunc, adUnitId: adUnitId, parent: self)
-    }
     
     public func presentInterstitialAd(isPresented: Binding<Bool>, adUnitId: String) -> some View {
-        FullScreenModifier(isPresented: isPresented, adType: .interstitial, rewardFunc: {}, adUnitId: adUnitId, parent: self)
+        FullScreenModifier(isPresented: isPresented, adUnitId: adUnitId, parent: self)
     }
 }
