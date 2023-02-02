369bdf9b7f1e362926011fc7314e9ce2728785e3
Andrey <andrey.grunyov@gmail.com>
2023-02-02 11:25:04 +0100

    back to List with fixed design

--- base/ChaptersList.swift
+++ base/ChaptersList.swift
@@ -16,57 +16,65 @@ struct ChaptersList: View {
     }
     
     var body: some View{
-        NavigationView {
-            VStack{
-                Text("CII R04 Pensions & Retirement Planning")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
+        VStack{
+            Text("CII R04 Pensions & Retirement Planning")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+            NavigationView {
+                List{
                     
-                
-                ForEach(chapters) { chapter in
                     
-                    ZStack {
-                        Button("") {self.showIntersitialAd.toggle()}
+                    
+                    ForEach(chapters) { chapter in
                         
-                        NavigationLink(destination:
-                                        StartTestView(chapter: chapter.title)
-                                       
-                        )
-                        {
-                            Text(chapter.title)
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding(6)
-                            //.scaledToFit()
-                            //.offset(y: 5)
-                                .frame(minWidth: 0, maxWidth: .infinity,  alignment: .leading
-                                )
+                        ZStack(alignment: .leading) {
+                            Button("") {self.showIntersitialAd.toggle()}
                             
-                            Spacer()
-                            
-                            
-                        }.background(Color(hex: "bad859"))
-                            .cornerRadius(10)
-                    }
-                 }.padding(1)
+                            NavigationLink(destination:
+                                            StartTestView(chapter: chapter.title)
+                                           
+                            )
+                            {
+                                Text(chapter.title)
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding(6)
+                                //.scaledToFit()
+                                //.offset(y: 5)
+                                    .frame(minWidth: 0, maxWidth: .infinity,  alignment: .leading
+                                    )
+                                
+                                
+                            }.background(Color(hex: "bad859"))
+                                .cornerRadius(10)
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                                .listRowInsets(EdgeInsets())
+                                .background(Color(UIColor.systemBackground))
+                        
+                    }.padding(5)
+                    
+                    Spacer()
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarTitle("Menu", displayMode: .inline)
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+                .listStyle(PlainListStyle())
+                //.listStyle(SidebarListStyle())
+                //.navigationBarHidden(false)
+                //.navigationBarBackButtonHidden(true)
+                //.hiddenNavigationBarStyle()
+                //.padding()
+                //.background(Color.green)
+                //remove the default Navigation Bar space:
+                //.hiddenNavigationBarStyle()
                 
-                Spacer()
             }
-            //.navigationBarTitleDisplayMode(.inline)
-            .navigationBarTitle("Menu", displayMode: .inline)
-            //.navigationBarHidden(false)
-            //.navigationBarBackButtonHidden(true)
-            
-            //.padding()
-                        //.background(Color.green)
-                        //remove the default Navigation Bar space:
-                        .hiddenNavigationBarStyle()
+            .presentInterstitialAd(isPresented: $showIntersitialAd,  adUnitId: "ca-app-pub-3940256099942544/4411468910")
             
         }
-        .presentInterstitialAd(isPresented: $showIntersitialAd,  adUnitId: "ca-app-pub-3940256099942544/4411468910")
-        
-        
     }
 }
 /*
